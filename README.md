50c776e52d0eacaeb75264ab176c0785a2a3696e
Brandon T. Fields <brandon@cdated.com>
2015-02-01 10:39:43 -0500

    Update README and script to show usage, add docstrings, move walk into function
    General documentation added into script to better explain functionality.

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 deps-to-dots
 ============
 
-Reads include statements in C and C++ code to generate a graphviz dot file.
+Reads #include statements in C and C++ code to generate a graphviz dot file.
+
+### Usage:
+
+The script starts with the main file and stays within the project.
+
+./make_dot.py [top_level_file] [project_root] > example.dot
--- make_dot.py
+++ make_dot.py
@@ -5,33 +5,34 @@ import re
 import os
 
 files_in_tree = {}
-inspect = {}
+inspected = {}
 
 include_regex = r'#include [<"](.*)[>"]'
 
 def inspect_file(file_name):
+    """ Recursively evaluate the file #includes and throughout the project. """
+
+    # Ensure the inspected files are within the project tree
     if not file_name in files_in_tree:
         return
 
     file_obj = open(files_in_tree[file_name])
 
-    inspect[file_name] = True
+    inspected[file_name] = True
     for line in file_obj:
         match = re.match(include_regex, line)
         if match:
             dep = match.group(1)
             print('\t"%s" -> "%s"' % (file_name, dep))
 
-            if not dep in inspect:
+            if not dep in inspected:
                 inspect_file(dep)
 
     file_obj.close()
 
-def main():
-    initial_file = sys.argv[1]
-    root_path = sys.argv[2]
-
-    files_in_tree[initial_file] = initial_file
+def record_project_files(root_path):
+    """ Walk the root path of the project to keep a dict of files within
+        the project. This keeps the script in the bounds of the project. """
 
     for root, dirs, files in os.walk(root_path):
         for name in files:
@@ -40,11 +41,32 @@ def main():
                 if not name in files_in_tree:
                     files_in_tree[name] = abs_file_path
 
+def usage():
+    """ Let the user know the expected runtime args """
+
+    if len(sys.argv) < 3:
+        print("Usage: ./make_dot.py [top_level_file] [project_root]")
+        sys.exit()
+
+def main():
+    usage()
+
+    initial_file = sys.argv[1]
+    root_path = sys.argv[2]
+
+    files_in_tree[initial_file] = initial_file
+    record_project_files(root_path)
+
     print('digraph "source tree" {')
+
+    # Start with the user specified main file
+    # and recurse through it's included files
     inspect_file(initial_file)
 
+    # Check the remaining files in the project
     for source_file in files_in_tree:
-        inspect_file(source_file)
+        if not source_file in inspected:
+            inspect_file(source_file)
     print('}')
 
 if __name__ == '__main__':
